d024f316950e3c5b59caf164cee153080248f671
szxivk <50396748+szxivk@users.noreply.github.com>
2025-02-06 07:00:01 +0000

    Add single round logic
    - announces winner of the round
    - stores score of the winner
    - tested through console.log

--- index.js
+++ index.js
@@ -19,3 +19,35 @@ let getHumanChoice = () => prompt("Choose rock, paper or scissors:");
 
 let humanScore = 0,
   computerScore = 0;
+
+// single round logic
+
+function playRound(c_choice, h_choice) {
+  h_choice = h_choice.toLowerCase();
+  if (c_choice === h_choice) {
+    console.log(`Nobody wins! Both chose ${c_choice}`);
+  } else if (c_choice === "rock" && h_choice === "paper") {
+    console.log(`You win! ${h_choice} beats ${c_choice}`);
+    return (humanScore += 1);
+  } else if (c_choice === "rock" && h_choice === "scissors") {
+    console.log(`You lose! ${c_choice} beats ${h_choice}`);
+    return (computerScore += 1);
+  } else if (c_choice === "paper" && h_choice === "scissors") {
+    console.log(`You win! ${h_choice} beats ${c_choice}`);
+    return (humanScore += 1);
+  } else if (c_choice === "paper" && h_choice === "rock") {
+    console.log(`You lose! ${c_choice} beats ${h_choice}`);
+    return (computerScore += 1);
+  } else if (c_choice === "scissors" && h_choice === "rock") {
+    console.log(`You win! ${h_choice} beats ${c_choice}`);
+    return (humanScore += 1);
+  } else if (c_choice === "scissors" && h_choice === "paper") {
+    console.log(`You lose! ${c_choice} beats ${h_choice}`);
+    return (computerScore += 1);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
