3d17ee75f2cbe0e019bea1b1dd4ba74a5b6d5113
szxivk <50396748+szxivk@users.noreply.github.com>
2025-02-06 05:59:43 +0000

    Add logic to get computer choice
    - updated index.js with getComputerChoice() func
    - tested the function using console.log

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+// Get computer choice, randomly choose rock, paper or scissor
+
+function getComputerChoice() {
+  const num = Math.floor(Math.random() * 10);
+  if (num <= 3) {
+    return "rock";
+  } else if (num > 3 && num < 6) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+// Get human choice
+// keep score for Both
