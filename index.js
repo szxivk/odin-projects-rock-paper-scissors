b94434e70f1a6ab08857ee1bc953fa2303330070
szxivk <50396748+szxivk@users.noreply.github.com>
2025-02-06 07:32:34 +0000

    Add logic for the 5 round game
    - also added a separate game winner announcement function which checks for the winner
    - tweaked some issues and tested through console.log
    - not adding mores stuff -in course I will have to revisit the project  -will add better logic/funcs at time

--- index.js
+++ index.js
@@ -22,32 +22,52 @@ let humanScore = 0,
 
 // single round logic
 
-function playRound(c_choice, h_choice) {
+function playRound(h_choice, c_choice) {
   h_choice = h_choice.toLowerCase();
   if (c_choice === h_choice) {
-    console.log(`Nobody wins! Both chose ${c_choice}`);
+    console.log(`Nobody wins! Both chose ${c_choice}.`);
   } else if (c_choice === "rock" && h_choice === "paper") {
-    console.log(`You win! ${h_choice} beats ${c_choice}`);
+    console.log(`You win! ${h_choice} beats ${c_choice}.`);
     return (humanScore += 1);
   } else if (c_choice === "rock" && h_choice === "scissors") {
-    console.log(`You lose! ${c_choice} beats ${h_choice}`);
+    console.log(`You lose! ${c_choice} beats ${h_choice}.`);
     return (computerScore += 1);
   } else if (c_choice === "paper" && h_choice === "scissors") {
-    console.log(`You win! ${h_choice} beats ${c_choice}`);
+    console.log(`You win! ${h_choice} beats ${c_choice}.`);
     return (humanScore += 1);
   } else if (c_choice === "paper" && h_choice === "rock") {
-    console.log(`You lose! ${c_choice} beats ${h_choice}`);
+    console.log(`You lose! ${c_choice} beats ${h_choice}.`);
     return (computerScore += 1);
   } else if (c_choice === "scissors" && h_choice === "rock") {
-    console.log(`You win! ${h_choice} beats ${c_choice}`);
+    console.log(`You win! ${h_choice} beats ${c_choice}.`);
     return (humanScore += 1);
   } else if (c_choice === "scissors" && h_choice === "paper") {
-    console.log(`You lose! ${c_choice} beats ${h_choice}`);
+    console.log(`You lose! ${c_choice} beats ${h_choice}.`);
     return (computerScore += 1);
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function winner(h_score, c_score) {
+  console.log("----> GAME <----");
+  if (h_score === c_score) {
+    return console.log(`Draw! Both scored ${h_score} points`);
+  } else if (h_score > c_score) {
+    return console.log(`--> You won! You scored +${h_score - c_score} points.`);
+  } else {
+    return console.log(
+      `--> You lost! Computer scored +${c_score - h_score} points. <--`
+    );
+  }
+}
 
-playRound(humanSelection, computerSelection);
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    console.log(`--> ROUND ${i + 1} <---`);
+    playRound(humanSelection, computerSelection);
+  }
+  return winner(humanScore, computerScore);
+}
+
+playGame();
