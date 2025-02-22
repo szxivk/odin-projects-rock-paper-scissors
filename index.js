d6ef2b219ed23e6681aad15f8dbbfaa68c54a08b
szxivk <50396748+szxivk@users.noreply.github.com>
2025-02-22 13:40:17 +0000

    Add buttons for user choice
    - reworked the whole thing -now using event listeners and event delegation
    - logic update
    - now first to 5 victories wins

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+let humanScore = 0,
+  computerScore = 0;
+
+const score = document.querySelector(".score");
+
+const options = document.querySelector(".buttons");
+
+const compChoice = document.querySelector();
+
+options.addEventListener("click", (e) => {
+  const target = e.target;
+  const computerChoice = getComputerChoice();
+  switch (target.id) {
+    case "rock":
+      playRound("rock", computerChoice);
+
+      if (humanScore >= 5 || computerScore >= 5) {
+        winner(humanScore, computerScore);
+      }
+      break;
+    case "paper":
+      playRound("paper", computerChoice);
+      if (humanScore >= 5 || computerScore >= 5) {
+        winner(humanScore, computerScore);
+      }
+      break;
+    case "scissors":
+      playRound("scissors", computerChoice);
+      if (humanScore >= 5 || computerScore >= 5) {
+        winner(humanScore, computerScore);
+      }
+      break;
+  }
+});
+
 // Get computer choice, randomly choose rock, paper or scissor
 
 function getComputerChoice() {
@@ -11,63 +46,43 @@ function getComputerChoice() {
   }
 }
 
-// Get human choice
-
-let getHumanChoice = () => prompt("Choose rock, paper or scissors:");
-
-// keep score for Both
-
-let humanScore = 0,
-  computerScore = 0;
-
 // single round logic
 
 function playRound(h_choice, c_choice) {
-  h_choice = h_choice.toLowerCase();
   if (c_choice === h_choice) {
     console.log(`Nobody wins! Both chose ${c_choice}.`);
   } else if (c_choice === "rock" && h_choice === "paper") {
     console.log(`You win! ${h_choice} beats ${c_choice}.`);
-    return (humanScore += 1);
+    humanScore += 1;
   } else if (c_choice === "rock" && h_choice === "scissors") {
     console.log(`You lose! ${c_choice} beats ${h_choice}.`);
-    return (computerScore += 1);
+
+    computerScore += 1;
   } else if (c_choice === "paper" && h_choice === "scissors") {
     console.log(`You win! ${h_choice} beats ${c_choice}.`);
-    return (humanScore += 1);
+
+    humanScore += 1;
   } else if (c_choice === "paper" && h_choice === "rock") {
     console.log(`You lose! ${c_choice} beats ${h_choice}.`);
-    return (computerScore += 1);
+
+    computerScore += 1;
   } else if (c_choice === "scissors" && h_choice === "rock") {
     console.log(`You win! ${h_choice} beats ${c_choice}.`);
-    return (humanScore += 1);
+
+    humanScore += 1;
   } else if (c_choice === "scissors" && h_choice === "paper") {
     console.log(`You lose! ${c_choice} beats ${h_choice}.`);
-    return (computerScore += 1);
+
+    computerScore += 1;
   }
 }
+
+// func to check whether score is above 5 or no
 
 function winner(h_score, c_score) {
-  console.log("----> GAME <----");
-  if (h_score === c_score) {
-    return console.log(`Draw! Both scored ${h_score} points`);
-  } else if (h_score > c_score) {
-    return console.log(`--> You won! You scored +${h_score - c_score} points.`);
+  if (h_score > c_score) {
+    alert("You Won! Refresh page to play again");
   } else {
-    return console.log(
-      `--> You lost! Computer scored +${c_score - h_score} points. <--`
-    );
+    alert("You Lose! Refresh page to play again");
   }
 }
-
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    console.log(`--> ROUND ${i + 1} <---`);
-    playRound(humanSelection, computerSelection);
-  }
-  return winner(humanScore, computerScore);
-}
-
-playGame();
